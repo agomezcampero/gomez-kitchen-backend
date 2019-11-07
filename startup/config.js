25b7c4f1dbc0f73afda1df32134446bdc3d90d11
agustin gomez <agustin@fanatiz.com>
2019-11-07 19:23:07 -0300

    added or for env variables

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,7 @@
 const config = require('config')
 
 module.exports = function(){
-  if (!config.get('jwtPrivateKey')){
+  if (!config.get('jwtPrivateKey') && !process.env.gomez-kitchen_jwtPrivateKey){
     throw new Error('FATAL ERROR: JWT Private Key is not defined')
   }
 }
