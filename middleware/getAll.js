cdcc4f9bde6e1acc517a5e6e484c100620664fb8
agustin gomez <agustin@fanatiz.com>
2019-11-19 17:54:07 -0300

    updated calculation of total pages

--- middleware/getAll.js
+++ middleware/getAll.js
@@ -1,20 +1,18 @@
 module.exports = async (req, Obj, filter) => {
-  const page = parseInt(req.query.page, 10) || 1
-  const itemsPerPage = parseInt(req.query.itemsPerPage, 10) || 10
-  const objects = await Obj
-    .find(filter)
+  const page = parseInt(req.query.page, 10) || 1;
+  const itemsPerPage = parseInt(req.query.itemsPerPage, 10) || 10;
+  const objects = await Obj.find(filter)
     .skip((page - 1) * itemsPerPage)
-    .limit(itemsPerPage)
+    .limit(itemsPerPage);
 
-  const totalItems = await Obj.countDocuments(filter)
-  const totalPages = Math.floor((totalItems - 1)/itemsPerPage + 1)
-  const items = objects.length 
+  const totalItems = await Obj.countDocuments(filter);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const items = objects.length;
 
-  const pagination = { page, items, totalPages, totalItems}
+  const pagination = { page, items, totalPages, totalItems };
 
   return {
     data: objects,
     pagination: pagination
-  }
-
-}
+  };
+};
